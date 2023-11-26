e4df0f6b1456064192bfa86fe56753703b72d031
Ferran Garcia <fgarcia@anybotics.com>
2023-11-26 20:40:29 +0100

    Added support for uploads

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // index.js
 const express = require('express')
+const fileUpload = require('express-fileupload');
 
 const app = express()
 const PORT = 4000
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+app.use(fileUpload());
+
+app.use(
+    fileUpload({
+        limits: {
+            fileSize: 10000000,
+        },
+        abortOnLimit: true,
+    })
+);
+
 
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
@@ -14,6 +23,27 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
+})
+
+app.post('/upload', (req, res) => {
+    // Get the file that was set to our field named "image"
+    const { image } = req.files;
+
+    // If no image submitted, exit
+    if (!image) return res.sendStatus(400);
+
+    // If does not have image mime type prevent from uploading
+    if (/^image/.test(image.mimetype)) return res.sendStatus(400);
+
+    // Move the uploaded image to our upload folder
+    image.mv(__dirname + '/upload/' + image.name);
+
+    // All good
+    res.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
 })
 
 // Export the Express API
